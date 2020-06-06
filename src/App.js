a5065e24a770ef30367037b75399a0da8c1265ae
Nathan Zhen <nathan@Nathans-MacBook-Pro.local>
2020-06-06 20:14:04 +1000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import ReactTooltip from "react-tooltip";
+import MapChart from "./MapChart";
 import './App.css';
 
 function App() {
+  const [content, setContent] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>World Map</h1>
+      <MapChart setTooltipContent={setContent} />
+      <ReactTooltip>{content}</ReactTooltip>
     </div>
   );
 }
---
+++ src/MapChart.js
@@ -0,0 +1,64 @@
+import React, { memo, Fragment } from 'react';
+import {
+  ZoomableGroup,
+  ComposableMap,
+  Geographies,
+  Geography
+} from 'react-simple-maps';
+
+const geoUrl =
+  'https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json';
+  
+const rounded = num => {
+  if (num > 1000000000) {
+    return Math.round(num / 100000000) / 10 + 'Bn';
+  } else if (num > 1000000) {
+    return Math.round(num / 100000) / 10 + 'M';
+  } else {
+    return Math.round(num / 100) / 10 + 'K';
+  }
+};
+
+const MapChart = ({ setTooltipContent }) => {
+  return (
+    <Fragment>
+      <ComposableMap data-tip='' height={350}>
+        <ZoomableGroup center={[15,0]} zoom={0.7}>
+          <Geographies geography={geoUrl}>
+            {({ geographies }) =>
+              geographies.map((geo) => (
+                <Geography
+                  key={geo.rsmKey}
+                  geography={geo}
+                  onMouseEnter={() => {
+                    const { NAME, POP_EST } = geo.properties;
+                    setTooltipContent(`${NAME} - ${rounded(POP_EST)}`);
+                  }}
+                  onMouseLeave={() => {
+                    setTooltipContent('');
+                  }}
+                  style={{
+                    default: {
+                      fill: '#D6D6DA',
+                      outline: 'none',
+                    },
+                    hover: {
+                      fill: '#F53',
+                      outline: 'none',
+                    },
+                    pressed: {
+                      fill: '#E42',
+                      outline: 'none',
+                    },
+                  }}
+                />
+              ))
+            }
+          </Geographies>
+        </ZoomableGroup>
+      </ComposableMap>
+    </Fragment>
+  );
+};
+
+export default memo(MapChart);
